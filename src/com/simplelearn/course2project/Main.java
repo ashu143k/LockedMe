48095fc4573254b8cfbebec0fafb308598160f84
ashu143k <ashish.pathak143k@gmail.com>
2022-09-21 14:42:26 +0530

    Version 1.1 Updation in SerachAFile Method

--- src/com/simplelearn/course2project/Main.java
+++ src/com/simplelearn/course2project/Main.java
@@ -13,9 +13,8 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("--------------- Welcome to Company Lockers Pvt. Ltd ---------------\n"
-				+ "Application Name : LockedMe.com\n"
-				+ "---------------Developer Details---------------\n" + "Name: Ashish Kumar Pathak \n"
-				+ "Designation: JAVA Developer\n" + "Date: 20-09-2022");
+				+ "Application Name : LockedMe.com\n" + "---------------Developer Details---------------\n"
+				+ "Name: Ashish Kumar Pathak \n" + "Designation: JAVA Developer\n" + "Date: 20-09-2022");
 
 		while (true) {
 			System.out.println("----------MAIN MENU----------");
@@ -42,6 +41,7 @@ public class Main {
 			}
 		}
 	}
+
 	private static void businessLevelOperation() throws IOException {
 		System.out.println("Select Any one to perform Business Level Operations");
 		System.out.println("Enter 1: To add a file");
@@ -70,27 +70,37 @@ public class Main {
 		default:
 			System.out.println("Kindly enter valid input");
 		}
-		if (businessLevelOperationChoice != 4 || businessLevelOperationChoice>4) {
+		if (businessLevelOperationChoice != 4 || businessLevelOperationChoice > 4) {
 			businessLevelOperation();
 		}
 	}
 
 	private static void searchAFile() {
-		System.out.println("Enter file name to search");
-		String searchFileName = null;
-		searchFileName = scan.next();
-		File searchFileDirectory = new File("C:\\Users\\ashis\\eclipse-workspace\\AssessmentProject\\Files\\" + searchFileName);
-		if (searchFileDirectory.exists()) {
-			System.out.println(searchFileName + " is available");
-		} else
-			System.out.println(searchFileName + " is not available");
-
+		File fileDir = new File("C:\\Users\\ashis\\eclipse-workspace\\AssessmentProject\\Files\\");
+		if (fileDir.isDirectory()) {
+			System.out.println("Enter file name to search");
+			String searchFileName = scan.next();
+			List<String> listFile = Arrays.asList(fileDir.list());
+			Iterator<String> searchIterate = listFile.iterator();
+			boolean flag = false;
+			while (searchIterate.hasNext()) {
+				String tempString = searchIterate.next().toString();
+				if (searchFileName.equalsIgnoreCase(tempString)) {
+					flag = true;
+				}
+			}
+			if (flag == true) {
+				System.out.println(searchFileName + " is available");
+			} else {
+				System.out.println(searchFileName + " is not available");
+			}
+		}
 	}
-
 	private static void deleteFile() {
 		System.out.println("Enter file name for delete");
 		String deleteFileName = scan.next();
-		File deleteFileDirectory = new File("C:\\Users\\ashis\\eclipse-workspace\\AssessmentProject\\Files\\" + deleteFileName);
+		File deleteFileDirectory = new File(
+				"C:\\Users\\ashis\\eclipse-workspace\\AssessmentProject\\Files\\" + deleteFileName);
 		if (deleteFileDirectory.delete())
 			System.out.println(deleteFileName + " is deleted");
 		else
@@ -123,5 +133,5 @@ public class Main {
 			System.out.println(fileDir.getAbsolutePath() + " is not a directory");
 		}
 	}
-	
+
 }
